6260c5eb7598a73f795370aa56c5c215e39aa2e6
Naomi <gashaija.naomi@gmail.com>
2025-02-24 16:18:56 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-System.out.println("Hello World");
+
+        System.out.println("Enter a first number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Enter a second number");
+        int b = sc.nextInt();
+        int Sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is " + Sum);
+
+
+
     }
 }
